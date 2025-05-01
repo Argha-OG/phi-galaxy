090eeac1020f96d244f86908ae0045f9a13bf8c1
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-05-02 04:09:55 +0600

    init

--- client/src/components/carousel/HCaro.jsx
+++ client/src/components/carousel/HCaro.jsx
@@ -13,7 +13,7 @@ import im5 from "./../../assets/caro/uidev.gif";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide,  } from 'swiper/react';
-import { EffectCoverflow, Pagination } from "swiper/modules";
+import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
 
 // Import Swiper styles
 import 'swiper/css';
@@ -21,6 +21,10 @@ import 'swiper/css';
 const HCaro = () => {
   return (
     <Swiper
+      autoplay={{
+        delay: 1500,
+        disableOnInteraction: false,
+      }}
       effect={"coverflow"}
       grabCursor={true}
       centeredSlides={true}
@@ -37,29 +41,6 @@ const HCaro = () => {
       modules={[EffectCoverflow, Pagination]}
       className="mySwiper"
     >
-      <SwiperSlide>
-        <div className="block rounded-lg bg-purple-700 p-2 shadow-indigo-100">
-          <a href="">
-            <img
-              alt=""
-              src={im1}
-              className="cardImage h-56 w-full rounded-md object-cover"
-            />
-
-            <div className="my-3 ml-3">
-              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
-                Java
-              </span>
-              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
-                FireBase
-              </span>
-              <h1 className="text-2xl mt-3 text-white font-semibold">
-                App Development
-              </h1>
-            </div>
-          </a>
-        </div>
-      </SwiperSlide>
       <SwiperSlide>
         <div className="block rounded-lg bg-purple-700 p-2 shadow-indigo-100">
           <a href="">
@@ -94,32 +75,6 @@ const HCaro = () => {
           <a href="">
             <img
               alt=""
-              src={im2}
-              className="cardImage h-56 w-full rounded-md object-cover"
-            />
-
-            <div className="my-3 ml-3">
-              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
-                Python
-              </span>
-              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
-                LLM
-              </span>
-              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
-                dJango
-              </span>
-              <h1 className="text-2xl mt-3 text-white font-semibold">
-                Artificial Intelligence
-              </h1>
-            </div>
-          </a>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="block rounded-lg bg-purple-700 p-2 shadow-indigo-100">
-          <a href="">
-            <img
-              alt=""
               src={im4}
               className="cardImage h-56 w-full rounded-md object-cover"
             />
@@ -129,9 +84,39 @@ const HCaro = () => {
                 Burp Suite
               </span>
               <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
+                Nmap
+              </span>
+              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
+                Nessus
+              </span>
+              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
+                MetaSploit
+              </span>
+              <h1 className="text-2xl mt-3 text-white">WebApp Security</h1>
+            </div>
+          </a>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div className="block rounded-lg bg-purple-700 p-2 shadow-indigo-100">
+          <a href="">
+            <img
+              alt=""
+              src={im5}
+              className="cardImage h-56 w-full rounded-md object-cover"
+            />
+
+            <div className="my-3 ml-3">
+              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
+                Java
+              </span>
+              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
                 FireBase
               </span>
-              <h1 className="text-2xl mt-3 text-white">App Development</h1>
+              <h1 className="text-2xl mt-3 text-white">
+                UI/UX & Graphics Design
+              </h1>
             </div>
           </a>
         </div>
@@ -152,7 +137,35 @@ const HCaro = () => {
               <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
                 FireBase
               </span>
-              <h1 className="text-2xl mt-3 text-white">App Development</h1>
+              <h1 className="text-2xl mt-3 text-white font-semibold">
+                App Development
+              </h1>
+            </div>
+          </a>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="block rounded-lg bg-purple-700 p-2 shadow-indigo-100">
+          <a href="">
+            <img
+              alt=""
+              src={im2}
+              className="cardImage h-56 w-full rounded-md object-cover"
+            />
+
+            <div className="my-3 ml-3">
+              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
+                Python
+              </span>
+              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
+                LLM
+              </span>
+              <span class="rounded-full mr-2  bg-white px-2.5 py-0.5 text-sm whitespace-nowrap text-purple-700">
+                dJango
+              </span>
+              <h1 className="text-2xl mt-3 text-white font-semibold">
+                Artificial Intelligence
+              </h1>
             </div>
           </a>
         </div>
