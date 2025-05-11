e3076718692a00a58599e797b3b959ff8eeacfe7
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-05-11 12:53:29 +0600

    choose us

--- client/src/pages/about/AboutUs.jsx
+++ client/src/pages/about/AboutUs.jsx
@@ -8,6 +8,11 @@ import Ico1 from "./../../assets/aboutpage/1.png";
 import Ico2 from "./../../assets/aboutpage/2.png";
 import Ico3 from "./../../assets/aboutpage/3.png";
 import Ico4 from "./../../assets/aboutpage/4.png";
+import ChoBack from "./../../assets/aboutpage/choback.png";
+import ChoHero from "./../../assets/aboutpage/chohero.png";
+import Busicon from "./../../assets/aboutpage/busicon.png";
+import Itcon from "./../../assets/aboutpage/itcon.png";
+
 
 
 
@@ -116,7 +121,59 @@ const AboutUs = () => {
         </div>
       </section>
 
-      
+      <section className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+          <div className="flex gap-4 content-center items-center my-16">
+            <div className="flex-1/2">
+              <div className="max-w-4/5 md:max-w-none">
+                <span className="font-bold text-3xl bg-gradient-to-r from-purple-700 to-blue-600 bg-clip-text text-transparent">
+                  Why Choose Us
+                </span>
+
+                <p className="text-lg max-w-4/5 mt-4 text-black">
+                  Choose PhyGalaxy for innovative IT solutions, expert
+                  consulting, and seamless digital transformation. We deliver
+                  secure, scalable, and future-ready technology to drive your
+                  business success
+                </p>
+
+                <div className="flex h-full w-full items-center content-center text-center justify-start m-2">
+                  <div className="flex items-center content-center text-center justify-center ">
+                    <img src={Busicon} alt="" />
+                    <div className="m-1">
+                      <strong>Business Groth</strong>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex h-full w-full items-center content-center text-center justify-start m-2">
+                  <div className="flex items-center content-center text-center justify-center">
+                    <img src={Itcon} alt="" />
+                    <div className="m-1">
+                      <strong>Technology Consultancy</strong>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex-1/2">
+              <div className="overflow-hidden flex justify-start h-full w-full">
+                <img
+                  src={ChoHero}
+                  className="relative right-30 h-[400px] w-auto shadow-2xs z-1"
+                  alt=""
+                />
+
+                <img
+                  src={ChoBack}
+                  className="absolute h-[400px] w-auto shadow-2xs ml-30"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </MainLayout>
   );
 };
